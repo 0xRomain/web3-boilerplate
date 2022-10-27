31122aa6fbf2355a9e36a4e0c168e916b62cd369
akugone <dev@akumaw.com>
2022-10-27 18:28:34 +0200

    add new page and wallet connect

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
 import { Web3Modal } from '@web3modal/react';
 import type { ConfigOptions } from '@web3modal/core';
+import Home from './pages/Home';
+import About from './pages/About';
+import Services from './pages/Services';
+import Dashboard from './pages/Dashboard';
 
 const config: ConfigOptions = {
   projectId: `${import.meta.env.VITE_WALLECT_CONNECT_PROJECT_ID}`,
@@ -18,17 +20,15 @@ const config: ConfigOptions = {
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <h1 className='text-3xl font-bold underline'>Hello Web3</h1>
+    <>
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='about' element={<About />} />
-        </Routes>
-      </header>
+        <Route path='/' element={<Home />} />
+        <Route path='about' element={<About />} />
+        <Route path='services' element={<Services />} />
+        <Route path='dashboard' element={<Dashboard />} />
+      </Routes>
       <Web3Modal config={config} />
-    </div>
+    </>
   );
 }
 
