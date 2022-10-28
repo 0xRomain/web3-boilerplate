53902445e5feea2aa32b9fbc609f36348ad67bd0
akugone <dev@akumaw.com>
2022-10-28 11:34:42 +0100

    Clean the notlog page

--- src/pages/NotLog.tsx
+++ src/pages/NotLog.tsx
@@ -19,22 +19,32 @@ function NotLog() {
   return (
     <>
       <Menu />
-      <div className='rounded-md bg-red-50 p-4'>
-        <div className='flex'>
-          <div className='flex-shrink-0'>
-            <XCircleIcon className='h-5 w-5 text-red-400' aria-hidden='true' />
-          </div>
-          <div className='ml-3'>
-            <h3 className='text-sm font-medium text-red-800'>
-            Please login to access your dashboard
-            </h3>
-            <div className='mt-2 text-sm text-red-700'>
-              <ul role='list' className='list-disc space-y-1 pl-5'>
-                <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit</li>
-                <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit</li>
-              </ul>
+      
+      <div className="min-h-full bg-white px-4 py-16 sm:px-6 sm:py-24 md:grid md:place-items-center lg:px-8">
+        <div className="mx-auto max-w-max">
+          <main className="sm:flex">
+            <p className="text-4xl font-bold tracking-tight text-indigo-600 sm:text-5xl">SORRY </p>
+            <div className="sm:ml-6">
+              <div className="sm:border-l sm:border-gray-200 sm:pl-6">
+                <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">You have to be connect</h1>
+                <p className="mt-1 text-base text-gray-500">to the Dapp to access your dashboard</p>
+              </div>
+              <div className="mt-10 flex space-x-3 sm:border-l sm:border-transparent sm:pl-6">
+                <a
+                  href="/"
+                  className="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                >
+                  Go back home
+                </a>
+                <a
+                  href=".about"
+                  className="inline-flex items-center rounded-md border border-transparent bg-indigo-100 px-4 py-2 text-sm font-medium text-indigo-700 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                >
+                  About
+                </a>
+              </div>
             </div>
-          </div>
+          </main>
         </div>
       </div>
       <Footer />
